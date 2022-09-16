6d83e0a3b624bb2bf3692639ee87fd291768f212
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-16 23:48:34 +0300

    Added the red and yellow when game start

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,11 +12,133 @@ const block9 = document.getElementById('block-9');
 const startBtn = document.getElementById('start-btn');
 const score = document.getElementById('score');
 
-const startGame = () => {
-    score.innerText='Game started';
+const insertCoin = () => {
+    // Game player Red=1 and yellow=0
+    let playerTurn = 1;
+    // block1
+    if(playerTurn==1){
+        block1.addEventListener('click',function(){
+            block1.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block1.addEventListener('click',function(){
+            block1.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block2
+    if(playerTurn==1){
+        block2.addEventListener('click',function(){
+            block2.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block2.addEventListener('click',function(){
+            block2.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block3
+    if(playerTurn==1){
+        block3.addEventListener('click',function(){
+            block3.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block3.addEventListener('click',function(){
+            block3.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block4
+    if(playerTurn==1){
+        block4.addEventListener('click',function(){
+            block4.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block4.addEventListener('click',function(){
+            block4.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block5
+    if(playerTurn==1){
+        block5.addEventListener('click',function(){
+            block5.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block5.addEventListener('click',function(){
+            block5.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block6
+    if(playerTurn==1){
+        block6.addEventListener('click',function(){
+            block6.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block6.addEventListener('click',function(){
+            block6.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block7
+    if(playerTurn==1){
+        block7.addEventListener('click',function(){
+            block7.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block7.addEventListener('click',function(){
+            block7.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block8
+    if(playerTurn==1){
+        block8.addEventListener('click',function(){
+            block8.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block8.addEventListener('click',function(){
+            block8.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+    // block9
+    if(playerTurn==1){
+        block9.addEventListener('click',function(){
+            block9.innerHTML='<img src="images/red.png" />';
+        });
+        playerTurn=0;
+    }
+    else{
+        block9.addEventListener('click',function(){
+            block9.innerHTML='<img src="images/yellow.png" />';
+        });
+        playerTurn=1;
+    }
+        
 }
-function insertCoin () {
-    
+
+
+const startGame = () => {
+    score.innerText='Game Started';
+    insertCoin();
 }
 
 startBtn.addEventListener('click',startGame);
